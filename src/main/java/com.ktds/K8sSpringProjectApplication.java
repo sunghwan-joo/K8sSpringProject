f64b7311b8fc04dfdc4a19beefa3021c500c9fba
Scaffolder <scaffolder@backstage.io>
2024-11-01 16:02:35 +0900

    initial commit

---
+++ src/main/java/com.ktds/K8sSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ktds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sSpringProjectApplication.class, args);
+	}
+
+}
